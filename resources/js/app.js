e195275cbdc811b263aff52a60aa00755daf9918
Tamrat Assefa Kessito <mamushassefa@gmail.com>
2021-12-02 13:46:32 +0300

    setup tailwind and vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-require('./bootstrap');
+require("./bootstrap");
+
+import Vue from "vue";
+
+Vue.component("qr-scanner", require("./components/QRScanner.vue").default);
+
+window.app = new Vue({
+    el: "#app",
+});
---
+++ resources/js/components/QRScanner.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="h-screen grid place-items-center">
+        <p
+            class="text-7xl bg-clip-text bg-gradient-to-r text-transparent font-bold from-indigo-500 to-purple-500"
+        >
+            this is a qr scanner
+        </p>
+    </div>
+</template>
